0cb79b7d700897ea42f03872b635b5463e301c61
jrball0904-beep <jrball0904@gmail.com>
2026-05-02 16:13:23 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-       <Text>Hai from jyoshna</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +18,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
